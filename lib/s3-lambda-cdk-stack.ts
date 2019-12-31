cb2ac2afd450b77235e2797eda25d2184314d5aa
Matthew Schrepel <matthew@extend.com>
2019-12-30 16:01:38 -0800

    Add first version after scaffold

--- lib/s3-lambda-cdk-stack.ts
+++ lib/s3-lambda-cdk-stack.ts
@@ -1,18 +1,12 @@
-import sns = require('@aws-cdk/aws-sns');
-import subs = require('@aws-cdk/aws-sns-subscriptions');
-import sqs = require('@aws-cdk/aws-sqs');
 import cdk = require('@aws-cdk/core');
+import s3 = require('@aws-cdk/aws-s3')
+import s3n = require('@aws-cdk/aws-s3-notifications')
 
 export class S3LambdaCdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'S3LambdaCdkQueue', {
-      visibilityTimeout: cdk.Duration.seconds(300)
-    });
+    const productCSVBucket = new s3.Bucket(this, 'product-csv-upload')
 
-    const topic = new sns.Topic(this, 'S3LambdaCdkTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
